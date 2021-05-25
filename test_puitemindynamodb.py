1c6d46d2f0e22bea5373c2cdf31395feb4a9000d
Syahirahaar <48470854+Syahirahaar@users.noreply.github.com>
2021-05-25 14:40:21 +0800

    -t3

--- test_puitemindynamodb.py
+++ test_puitemindynamodb.py
@@ -10,5 +10,3 @@ def lambda_handler(event, context):
     input = {'TransactionId' : '31', 'State' : 'PENDING', 'RelatedTransactions' : [32,33,34], 'CustomerDetails' : {'Name' :'John Doe', 'AccountBalance' : 50}}
     input = {'TransactionType_OriginCountry':'PURCHASE_USA', 'Date' : '2019-10-22'}
     response = table.put_item(Item=input)
-
-dfbfjfbsf
