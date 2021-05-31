801c6dd6dd5cd4c64e4083dba30f0743d35de62a
Syahirahaar <48470854+Syahirahaar@users.noreply.github.com>
2021-05-31 09:08:22 +0800

    31may21

--- insert_template.py
+++ insert_template.py
@@ -2,14 +2,39 @@ import pandas as pd
 import requests
 
 #URL OF ENDPOINT
-URL =
+URL = "https://p6pq7a2yf5.execute-api.ap-southeast-1.amazonaws.com/test"
 
 #read the local file
-data = pd.read_cvs('airline.csv'), sep=','
+data = pd.read_csv('sample_data.csv', sep=',')
+
+# write a single row from the testfile into the api
+#export = data.loc[2].to_json()
+#response = requests.post(URL, data = export)
+#print(response)
+
+# write all the rows from the testfile to the api as put request
+
+# for i in data.index:
+#         # convert the row to json
+#     export = data.loc[i].to_json()
+#
+#         #send it to the api
+#     response = requests.post(URL, data = export)
+#
+#         # print the returncode
+#
+#     print(response)
 
 for i in data.index:
-    export = data.loc[i].to_json()
+    try:
+        # convert the row to json
+        export = data.loc[i].to_json()
 
-    response = requests.post(URL,data=export)
+        #send it to the api
+        response = requests.post(URL, data = export)
 
-    print(response)
+        # print the returncode
+        print(export)
+        print(response)
+    except:
+        print(data.loc[i])
