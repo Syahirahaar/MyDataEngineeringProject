7bcd890facdf63ddd21fc398cc7b1a038f5a4546
Syahirahaar <48470854+Syahirahaar@users.noreply.github.com>
2021-06-16 09:04:29 +0800

    15june21

--- test_dynamodbtolocal.py
+++ test_dynamodbtolocal.py
@@ -1,4 +1,7 @@
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
+access_key = 'AKIA6EBJ5GBWCVPKJ6VR'
+secret_key = '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'
+
+dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-1',aws_access_key_id=access_key, aws_secret_access_key = secret_key)
 list(dynamodb.tables.all())
